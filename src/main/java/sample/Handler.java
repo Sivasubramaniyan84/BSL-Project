9f06b71dd7369eb88c560d653ca6989b8a3cc90a
Sivasubramaniyan Sivakumar <sivasubramaniyansivakumar@Sivasubramaniyans-Mac-mini.local>
2023-07-06 22:07:10 -0400

    Updated

--- src/main/java/sample/Handler.java
+++ src/main/java/sample/Handler.java
@@ -1,4 +1,15 @@
 package sample;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Handler {
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.logging.Logger;
+public class Handler implements RequestHandler<Map<String, String>, String> {
+    private static final Logger LOGGER = Logger.getLogger(Handler.class.getName());
+    public String handleRequest(Map <String,String> values, Context context) {
+        LOGGER.info("Inside handleRequest () >>>>");
+        return "Invoking Lambda";
+    }
+
 }

a83e034821cc77489a3cc25eabe02971620e1597
Sivasubramaniyan Sivakumar <sivasubramaniyansivakumar@Sivasubramaniyans-Mac-mini.local>
2023-07-06 22:30:05 -0400

    Updated

--- src/main/java/sample/RequestFunctionHandler.java
+++ src/main/java/sample/RequestFunctionHandler.java
@@ -1,15 +1,16 @@
 package sample;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.logging.Logger;
-public class Handler implements RequestHandler<Map<String, String>, String> {
-    private static final Logger LOGGER = Logger.getLogger(Handler.class.getName());
-    public String handleRequest(Map <String,String> values, Context context) {
-        LOGGER.info("Inside handleRequest () >>>>");
-        return "Invoking Lambda";
-    }
+public class RequestFunctionHandler implements RequestHandler <Map<String,String>,String> {
+    private static final Logger LOGGER = Logger.getLogger(RequestFunctionHandler.class.getName());
 
+    public String handleRequest(Map <String,String> values, Context context) {
+        LOGGER.info("Inside handleRequest");
+        return "Invoking Lambda Functions";
+    }
 }

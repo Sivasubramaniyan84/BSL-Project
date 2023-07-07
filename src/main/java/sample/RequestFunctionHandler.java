9c49e378dc1264ebeca6d1a9f6635615f2f7672c
Sivasubramaniyan Sivakumar <sivasubramaniyansivakumar@Sivasubramaniyans-Mac-mini.local>
2023-07-07 09:43:09 -0400

    Updated Handler

--- src/main/java/sample/RequestFunctionHandler.java
+++ src/main/java/sample/RequestFunctionHandler.java
@@ -1,16 +1,43 @@
 package sample;
 
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
+import software.amazon.awssdk.services.s3.S3Client;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
 
-import java.math.BigDecimal;
 import java.util.Map;
 import java.util.logging.Logger;
-public class RequestFunctionHandler implements RequestHandler <Map<String,String>,String> {
+public class RequestFunctionHandler implements RequestHandler <S3Event, String> {
     private static final Logger LOGGER = Logger.getLogger(RequestFunctionHandler.class.getName());
-
-    public String handleRequest(Map <String,String> values, Context context) {
+    @Override
+    public String handleRequest(S3Event s3event, Context context) {
         LOGGER.info("Inside handleRequest");
-        return "Invoking Lambda Functions - 123";
+        try {
+            S3EventNotificationRecord record = s3event.getRecords().get(0);
+            String srcBucket = record.getS3().getBucket().getName();
+            String srcKey = record.getS3().getObject().getUrlDecodedKey();
+
+            S3Client s3Client = S3Client.builder().build();
+            HeadObjectResponse headObject = getHeadObject(s3Client, srcBucket, srcKey);
+
+            LOGGER.info("Successfully retrieved " + srcBucket + "/" + srcKey + " of type " + headObject.contentType());
+
+            return "Ok";
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    private HeadObjectResponse getHeadObject(S3Client s3Client, String bucket, String key) {
+        HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+        return s3Client.headObject(headObjectRequest);
+    }
+
 }
